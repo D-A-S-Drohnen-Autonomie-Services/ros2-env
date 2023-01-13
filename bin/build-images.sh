de66ad93f90981ec0069138d0ed1e9552ee27c59
Andrew Pettey <andrew@pettey.me>
2023-01-13 15:56:03 +0100

    Camera lesson (#8)
    * chore:cleaned up repo
    
    * chore: clean up docker images and docker-compose
    
    * chore: learning about intra-process communication https://docs.ros.org/en/humble/Tutorials/Demos/Intra-Process-Communication.html
    
    * chore: fixing src
    
    * chore: added src back
    
    * feature: added optical flow and edge detection with a launcher
    
    * chore: added makefile shell to use bash
    
    * chore: updated implementation of build image
    
    * chore: fixed arm builds
    
    * chore: commit

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 docker build docker/base-image/. -t das:ros2-base
 docker build docker/rqt/. -t das:ros2-rqt
+docker buildx build --platform linux/amd64,linux/arm64 -t petdog/das:latest-base --push -f docker/base-image/Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t petdog/das:latest --push -f docker/built-application/Dockerfile .
+
+docker manifest create \
+petdog/das:latest \
+--amend petdog/das:latest-amd64 \
+--amend petdog/das:latest-arm64v8
